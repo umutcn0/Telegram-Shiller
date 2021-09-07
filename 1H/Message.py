63bd2b7e3adb7f6b2d8b1217445d4de1dcb3267e
Umut Can <54486428+umutcn0@users.noreply.github.com>
2021-09-07 18:22:05 +0300

    Update Message.py

--- 1H/Message.py
+++ 1H/Message.py
@@ -1,14 +1,11 @@
 def fetch_text():
-    self = """Fair Launch 🚀🚀\n
-Doge Reward, No NEED To Wait\n\n
-TG : @bonusdogebsc
-"""
+    self = """
+""" # Your Message here
     return self
 
 
 def fetch_list():
-    channel_list = ['uniswapgemtargets','infocryptoproject','bscdubai','bscspain','bscjapan'
-        ,'bscturkey','bscitaly','bscnigeria','bscchina','bscindia','bscgermany','rugorriches','chatbsc','bscdegens']
+    channel_list = [] #Channel list here
 
     return channel_list
 
